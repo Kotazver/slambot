5e35fdbe6f67ad2ce30d48a28e2bd97607428731
Kotazver <maksimtimakov904@gmail.com>
2026-06-01 15:52:48 +0300

    added connection to database

--- src/main.lua
+++ src/main.lua
@@ -1,4 +1,8 @@
 local api = require("telegram-bot-lua").configure(os.getenv("telegram_api_token"))
+local utils = require("src.utils")
+local db = api.db.connect({ driver = "sqlite", path = "db/slambot.db"})
+
+
 
 function api.on_message(msg)
 
